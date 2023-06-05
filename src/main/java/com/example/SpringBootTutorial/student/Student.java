e9e1ee185ec8e97d3365daac3004d7df0d04b4bc
digitalhoboxx <stanalin91@gmail.com>
2023-06-05 13:55:01 +0300

    implemented transient logic for age

--- src/main/java/com/example/SpringBootTutorial/student/Student.java
+++ src/main/java/com/example/SpringBootTutorial/student/Student.java
@@ -3,6 +3,7 @@ package com.example.SpringBootTutorial.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 //Student class is mapped to student table in DB
 @Entity
@@ -22,21 +23,20 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Student() {
@@ -75,7 +75,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
