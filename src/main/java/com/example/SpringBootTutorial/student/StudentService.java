5551443d78ee349b3914ef38edcaeeeb1916d390
digitalhoboxx <stanalin91@gmail.com>
2023-06-05 15:07:41 +0300

    added update student logic

--- src/main/java/com/example/SpringBootTutorial/student/StudentService.java
+++ src/main/java/com/example/SpringBootTutorial/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.SpringBootTutorial.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -40,5 +42,25 @@ public class StudentService {
             );
         }
         studentRepository.deleteById(studentId);
+    }
+
+    //will send the update automatically to db
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id" + studentId + "does not exist"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
