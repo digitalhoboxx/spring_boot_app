c5c9a45f5122b722851e7af5dc1c3607e80bc622
digitalhoboxx <stanalin91@gmail.com>
2023-06-05 14:25:01 +0300

    added check for duplicate email logic

--- src/main/java/com/example/SpringBootTutorial/student/StudentRepository.java
+++ src/main/java/com/example/SpringBootTutorial/student/StudentRepository.java
@@ -1,10 +1,17 @@
 package com.example.SpringBootTutorial.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
+
+    // line below could be commented out too, this way the query is more specific
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
