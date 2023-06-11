017e2ababb59172cecc22d0be575305c86dcf58d
digitalhoboxx <stanalin91@gmail.com>
2023-06-11 11:02:39 +0300

    updated to jdk 17(azul)

--- src/main/java/com/example/SpringBootTutorial/siteUser/SiteUserController.java
+++ src/main/java/com/example/SpringBootTutorial/siteUser/SiteUserController.java
@@ -1,31 +1,31 @@
-package com.example.SpringBootTutorial.siteUser;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(path= "api/v1/siteuser")
-public class SiteUserController {
-    private final SiteUserService siteUserService;
-
-    @Autowired
-    public SiteUserController(SiteUserService siteUserService) {
-        this.siteUserService = siteUserService;
-    }
-
-    @GetMapping
-    public SiteUser getSiteUsers(){
-        return siteUserService.getSiteUsers;
-    }
-
-    @PostMapping
-    public void registerNewSiteUser(){
-        siteUserService.addNewSiteUser;
-    }
-
-    @DeleteMapping(path="rm/{siteUserId}")
-    public void deleteSiteUser(){
-        siteUserService.deleteSiteUser(@PathVariable("siteUserId") Long siteUserId) { siteUserService.deleteSiteUser(siteUserId);
-        }
-    }
-}
+//package com.example.SpringBootTutorial.siteUser;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping(path= "api/v1/siteuser")
+//public class SiteUserController {
+//    private final SiteUserService siteUserService;
+//
+//    @Autowired
+//    public SiteUserController(SiteUserService siteUserService) {
+//        this.siteUserService = siteUserService;
+//    }
+//
+//    @GetMapping
+//    public SiteUser getSiteUsers(){
+//        return siteUserService.getSiteUsers;
+//    }
+//
+//    @PostMapping
+//    public void registerNewSiteUser(){
+//        siteUserService.addNewSiteUser;
+//    }
+//
+//    @DeleteMapping(path="rm/{siteUserId}")
+//    public void deleteSiteUser(){
+//        siteUserService.deleteSiteUser(@PathVariable("siteUserId") Long siteUserId) { siteUserService.deleteSiteUser(siteUserId);
+//        }
+//    }
+//}
